d5bb4d21b21bec7980c3e87e705451351ca7c802
SergioGeorge <a01365942@itesm.mx>
2020-02-28 09:02:04 -0600

    Detect NUM, +, ERROR of NUM

--- tokens.py
+++ tokens.py
@@ -1,22 +1,19 @@
 import os
 import sys
+#from globalTypes import TokenType
 
-mainFile = os.getcwd() + "/matriz2.txt"
-
-# Metodos
-with open(mainFile) as f:
-    simbolos = next(f).split(',')
+p = 0 #posicion del puntero en el documento
+#mainFile = os.getcwd() + "/matrix_csv.txt"
+with open("./clean_csv/output/matrix_csv.txt") as f:
+    simbolos = next(f).split('_')
     M = [[int(x) for x in line.split()] for line in f]
-
-#print(simbolos)
-#print(M)
-
 f = open('./input.txt', 'r')
 archivo = f.read() 		# lee todo el archivo a tokenizar
-archivo += '$'                  # agregamos $ para representar EOF
+
+#archivo += '$'                  # agregamos $ para representar EOF
 longitud = len(archivo) 	# longitud del archivo
-estado = 0                      # estado inicial
-token = ''                      # token inicial
+estado = 0
+token = ''
 
 mapa = {}
 # Creamos un array(llamado mapa) que clasifica/separa los 'digitos' asignandoles un 0
@@ -24,129 +21,44 @@ mapa = {}
 # e.g mapa = [0,0,0,..., (10)0, 1, 1, 1]
 for i in range(len(simbolos)):
     for c in simbolos[i]:# recooremos primero digitos, luego alfabeto
-        mapa[c]=i
+        mapa[c]=i+1
 
-p = 0
-isScientificNotation = False
+def getToken(archivo):
+    global p
+    global estado
+    global token
+    global mapa
 
-print("Token     Tipo")
+   
+    token = ''
 
-while p < longitud :
-    c = archivo[p] # Leemos cada caracter del archivo 'ejemplo.txt'
-    estado = M[estado][mapa[c]]# Matriz que representa la funcion de transicion
-    if estado == 2: # Estado de aceptacion de token
-        if isScientificNotation:
-            print("{}       Real".format(token))
-            isScientificNotation = False
-        else:
-            print("{}       Entero".format(token))
-        token = '' # Limpiamos el token
-        estado = 0 # Volvemos al estado inicial
-        p = p - 1
-    elif estado == 4:#Punto
-        token += c
-        estado = estado - 1
-        isScientificNotation = True
-    elif estado == 5: # Mostramos el num real
-        if str(c).isdigit():
-            token += c
-        else:
-            p -= 1
-        print("{}       Real".format(token))
-        token = ''
-        estado = 0
-    elif estado == 6: #Suma
-        token += c
-        print('{}       Suma'.format(token)) 
-        token = ''
-        estado = 0
-    elif estado == 7: #Resta
-        token += c
-        print('{}       Resta'.format(token))
-        token = ''
-        estado = 0
-    elif estado == 8: #Mult
-        token += c
-        print("{}       Multiplicacion".format(token))
-        token = ''
-        estado = 0 
-    elif estado == 9: #Division
-        token += c
-        if archivo[p+1] == '/':
-            estado = 6
-        else:
-            print('{}       Division'.format(token))
-            token = ''
-            estado = 0
-    elif estado == 10: # espacios en blanco
-        estado = 0
-    elif estado == 11: # Igual/Asignacion
-        if c != '\n':
-            token += c
-            print('{}       Asignacion'.format(token))
-        token = ''
-        estado = 0
-    elif estado == 12: # Parentesis izquierdo
-        if c != '\n':
-            token += c
-            print('{}       Parentesis que abre'.format(token))
-        token = ''
-        estado = 0
-    elif estado == 13: # Parentesis Derecho
-        if c != '\n':
-            token += c
-            print('{}       Parentesis que cierra'.format(token))
-        token = ''
-        estado = 0
-    elif estado == 14: #Auxiliar (Notacion Cientifica)
-        token += c
-        estado = 4
-    elif estado == 15: #Auxiliar (Notacion Cientifica)
-        token+=c
-        estado = 5
-    elif estado == 16: # Comentario (Se entra por estado 9)
-        while p < len(archivo):
-            c = archivo[p]
+    while p < longitud :
+        c = archivo[p] # Leemos cada caracter del archivo 'ejemplo.txt'     # llega ' ',5
+        #debug
+        estadoAntiguo = estado
+        simboloActual = mapa[c]
 
-            if c != '\n' and c != '$':
-                token += c
-            else:
-                print("{}       Comentario".format(token))
-                token = ''
+        estado = M[estado][mapa[c]]# Matriz que representa la funcion de transicion # estado = 0 mapa[c] = 0, estado = 1 mapa[c] = 1
+        if estado == 2: # Estado de aceptacion de token
                 estado = 0
-                break
-            p += 1
-    elif estado == 17:
-        token += c
-        c = archivo[p+1]
-        if c.isalpha() or c.isdigit() or c == '_':
-            estado = 7
-        else:
-            print('{}       Variable'.format(token))
-            estado = 0
-            token = ''
-    elif estado == 18:#Variable con mas de un caracter
-        while p < len(archivo):
-            c = archivo[p]
-
-            if c.isdigit() or c.isalpha() or c == '_':
-                token += c
-            else:
+                return token, p
+        elif estado == 15:
+                p = p + 1
                 estado = 0
-                print("{}       Variable".format(token))
-                p -= 1
-                token = ''
-                break
-            p += 1
-    elif estado == 19: #^
-        token += c
-        print('{}       Potencia'.format(token))
-        token = ''
-        estado = 0 
-    elif estado == 20:
-        print("Error")
-    elif estado != 0:# Si el caracter es distinto del blanco lo concatenamos con el caracter anterior para formar un token
-        if c != '\n':
-            token += c
-    p+=1
-        
+                token = '+'
+                return token, p 
+        elif estado == 50:
+            print("Error en: ", p)
+            sys.exit()
+        elif estado != 0:# Si el caracter es distinto del blanco lo concatenamos con el caracter anterior para formar un token
+            if c != '\n':
+                token += c
+        p+=1
+
+
+i = 0 
+longitud = longitud 
+while p < longitud:
+    token, p =getToken(archivo)
+    print(token)
+    print("posicion ", p)
