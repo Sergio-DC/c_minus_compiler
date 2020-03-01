5592c454ebf7b1367806c8322f701ab7bb1764a2
SergioGeorge <a01365942@itesm.mx>
2020-03-01 14:59:27 -0600

    The 'lexer.py' professor testing enviroment was recreated
    Introducing a new file: test.py
    Using the Enum class TokenType
    
    Miss include reserved words, coming soon!!

--- lexer.py
+++ lexer.py
@@ -1,226 +1,233 @@
-import os
 import sys
 from globalTypes import TokenType
 
-posicion = 0 #posicion del puntero en el documento
-#mainFile = os.getcwd() + "/matrix_csv.txt"
 with open("./clean_csv/output/matrix_csv.txt") as f:
     simbolos = next(f).split('_')
     M = [[int(x) for x in line.split()] for line in f]
-f = open('./input.txt', 'r')
-programa = f.read() 		# lee todo el programa a tokenizar
 
-programa += '$'                  # agregamos $ para representar EOF
-progLong = len(programa) 	# progLong del programa
 estado = 0
-token = ''
-
 mapa = {}
 # Creamos un array(llamado mapa) que clasifica/separa los 'digitos' asignandoles un 0
 # Al alfabeto y underscore
 # e.g mapa = [0,0,0,..., (10)0, 1, 1, 1]
 for i in range(len(simbolos)):
-    for c in simbolos[i]:# recooremos primero digitos, luego alfabeto
+    for c in simbolos[i]:# recorremos primero digitos, luego alfabeto
         mapa[c]=i+1
 
-def getToken():
+def globales(prog,pos,long):
     global programa
-    global posicion
-    global estado
-    global token
-    global mapa
     global progLong
+    global posicion
+    programa = prog
+    posicion = pos
+    progLong = long
 
-   
-    token = ''
-
-    while posicion < progLong :
+def getToken(imprime = True):
+    global estado, posicion
+    tokenAppend = ''
+    global mapa
+    
+    while posicion <= (progLong + 1) :
         c = programa[posicion] # Leemos cada caracter del programa 'ejemplo.txt'     # llega ' ',5
         #debug
         estadoAntiguo = estado
         simboloActual = mapa[c]
+        tokenReconocido = False
 
         estado = M[estado][mapa[c]]# Matriz que representa la funcion de transicion # estado = 0 mapa[c] = 0, estado = 1 mapa[c] = 1
         if estado == 2: # Estado de aceptacion de token
-                estado = 0
-                return TokenType.NUM.name, token
+                tokenReconocido = True
+                token = TokenType.NUM.name
+                tokenString = tokenAppend
         elif estado == 15:
                 posicion = posicion + 1
-                estado = 0
-                token +=c
-                return TokenType.PLUS.name, TokenType.PLUS.value
+                tokenAppend +=c
+                tokenReconocido = True
+                token = TokenType.PLUS.name
+                tokenString = TokenType.PLUS.value
         elif estado == 16:
                 posicion = posicion + 1
-                estado = 0
-                token = '-'
-                return TokenType.MINUS.name, TokenType.MINUS.value
+                tokenAppend = '-'
+                tokenReconocido = True
+                token = TokenType.MINUS.name
+                tokenString = TokenType.MINUS.value
         elif estado == 17:
                 posicion = posicion + 1
-                estado = 0
-                token = '*'
-                return TokenType.MULT.name, TokenType.MULT.value
+                tokenAppend = '*'
+                tokenReconocido = True
+                token = TokenType.MULT.name
+                tokenString = TokenType.MULT.value
         elif estado == 18:
-                token += c
+                tokenAppend += c
                 posicion += 1
                 c = programa[posicion];
                 if c == '*': # It's a block comment
-                    token += c
+                    tokenAppend += c
                     estado = 3 # Real state x
                 else:
-                    token += c #DIV
-                    estado = 0
-                    return TokenType.DIV.name, TokenType.DIV.value
+                    tokenAppend += c #DIV
+                    tokenReconocido = True
+                    token = TokenType.DIV.name
+                    tokenStringTokenType.DIV.value
         elif estado == 19:
             if c == '*':
-                token += c
+                tokenAppend += c
                 posicion = posicion + 1
                 estado = 3 # Real State x
             else:
-                token += c
+                tokenAppend += c
                 estado = 3 #Real state 19
         elif estado == 20:
-            token += c
+            tokenAppend += c
             posicion += 1
             c = programa[posicion]
             if c == '/':
-                token += c
+                tokenAppend += c
                 posicion+=1
-                estado = 0
-                return TokenType.COMMENT.name, token # go to State 21 and return comment
+                token = TokenType.COMMENT.name
+                tokenString = tokenAppend # go to State 21 and return comment
             elif c == '*':
-                token += c
+                tokenAppend += c
                 estado = 4 # go to state 20
             else:
-                token += c
+                tokenAppend += c
                 estado = 3
         elif estado == 22:# DIV
-            estado = 0
-            token = '/';
-            return TokenType.DIV.name, TokenType.DIV.value
+            token = TokenType.DIV.name
+            tokenString = TokenType.DIV.value
         elif estado == 23:
-            token += c
+            tokenAppend += c
             posicion += 1
             c = programa[posicion];
             # go to state 25
             if c == '=': # '<=' token
-                token += c
+                tokenAppend += c
                 posicion += 1
-                estado = 0 
-                return TokenType.LET.name, TokenType.LET.value
+                tokenReconocido = True
+                token = TokenType.LET.name
+                tokenString = TokenType.LET.value
             else: # go to state 24 LT '<'
-                estado = 0
-                return TokenType.LT.name, TokenType.LT.value
+                tokenReconocido = True
+                token = TokenType.LT.name
+                tokenString = TokenType.LT.value
         elif estado == 26:
-            token += c
+            tokenAppend += c
             posicion += 1
             c = programa[posicion];
             # go to state 28
             if c == '=': # '>=' token
-                token += c
-                posicion += 1
-                estado = 0 
-                return TokenType.GET.name, TokenType.GET.value
+                tokenAppend += c
+                posicion += 1 
+                tokenReconocido = True
+                token = TokenType.GET.name
+                tokenString = TokenType.GET.value
             else: # go to state 27
-                estado = 0
-                return TokenType.GT.name, TokenType.GT.name
+                tokenReconocido = True
+                token = TokenType.GT.name
+                tokenString = TokenType.GT.name
         elif estado == 29:
-            token +=c
+            tokenAppend +=c
             c = programa[posicion + 1]
             if c.isalpha():
                 estado = 7 # Go to state 29
             else:
-                estado = 0
                 posicion += 1
-                return TokenType.ID.name, token # token ID, state 30
+                tokenReconocido = True
+                token = TokenType.ID.name # token ID, state 30
+                tokenString = tokenAppend 
         elif estado == 31:
-            token += c
+            tokenAppend += c
             posicion += 1
             c = programa[posicion];
             # go to state 33
             if c == '=': # '==' token
-                token += c
+                tokenAppend += c
                 posicion += 1
-                estado = 0 
-                return TokenType.EQ.name, TokenType.EQ.value
+                tokenReconocido = True
+                token = TokenType.EQ.name
+                tokenString = TokenType.EQ.value
             else: # go to state 32 "ASSIGN"
-                estado = 0
-                return TokenType.ASSIGN.name, TokenType.ASSIGN.value
+                tokenReconocido = True
+                token = TokenType.ASSIGN.name
+                tokenString = TokenType.ASSIGN.value
         elif estado == 34:
-            token += c
+            tokenAppend += c
             posicion += 1
             c = programa[posicion];
             # go to state 35
             if c == '=': # '!=' token
-                token += c
+                tokenAppend += c
                 posicion += 1
-                estado = 0 
-                return TokenType.NOT_EQ.name, TokenType.NOT_EQ.value
+                tokenReconocido = True
+                token = TokenType.NOT_EQ.name
+                tokenString = TokenType.NOT_EQ.value
             else: # go to state of error
-                estado = 0
                 token = "Error en !"
                 return token, posicion
         elif estado == 36: #Token ';'
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.SEMI.name, TokenType.SEMI.value
+            tokenReconocido = True
+            token = TokenType.SEMI.name
+            tokenString = TokenType.SEMI.value
         elif estado == 37: # Token ','
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.COMMA.name, TokenType.COMMA.value
+            tokenReconocido = True
+            token = TokenType.COMMA.name
+            tokenString = TokenType.COMMA.value
         elif estado == 38: # Token '('
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.LPAREN.name, TokenType.LPAREN.value
+            tokenReconocido = True
+            token = TokenType.LPAREN.name
+            tokenString = TokenType.LPAREN.value
         elif estado == 39: # Token ')'
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.RPAREN.name, TokenType.RPAREN.value
+            tokenReconocido = True
+            token = TokenType.RPAREN.name
+            tokenString = TokenType.RPAREN.value
         elif estado == 40: # Token '['
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.LBRACKET.name, TokenType.LBRACKET.value
+            tokenReconocido = True
+            token = TokenType.LBRACKET.name
+            tokenString = TokenType.LBRACKET.value
         elif estado == 41: # Token ']'
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.RBRACKET.name, TokenType.RBRACKET.value
+            tokenReconocido = True
+            token = TokenType.RBRACKET.name
+            tokenString = TokenType.RBRACKET.value
         elif estado == 42: # Token '{'
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.LBRACE.name, TokenType.LBRACE.value
+            tokenReconocido = True
+            token = TokenType.LBRACE.name
+            tokenString = TokenType.LBRACE.value
         elif estado == 43: # Token '}'
-            token += c
+            tokenAppend += c
             posicion += 1
-            estado = 0
-            return TokenType.RBRACE.name, TokenType.RBRACE.value
+            token = TokenType.RBRACE.name
+            tokenString = TokenType.RBRACE.value
+            tokenReconocido = True
         elif estado == 44:#EOF '$'
-            token += c
+            tokenAppend += c
             posicion += 1
-            return TokenType.ENDFILE, TokenType.ENDFILE.value
+            tokenReconocido = True
+            token = TokenType.ENDFILE
+            tokenString = TokenType.ENDFILE.value
         elif estado == 50:
             print("Error en: ", posicion)
             sys.exit()
         elif estado != 0:# Si el caracter es distinto del blanco lo concatenamos con el caracter anterior para formar un token
             if c != '\n':
-                token += c
-            elif c == '$':
-                return token, posicion
+                tokenAppend += c
+                
+        if(tokenReconocido):
+            estado = 0
+            if imprime:
+                print(token," = ", tokenString)
+            return token, tokenString
         posicion+=1
-
-# class States(Enum):
-#     s18 = 2
-#     s19 = 4
-
-
-
-progLong = progLong 
-token, tokenString =getToken()
-while token != TokenType.ENDFILE:
-    print("{}   {}".format(token,str(tokenString)))
-    token, tokenString =getToken()    
